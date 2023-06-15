13dc8fafe8478ea1df7a951404cb0e0033ca0a93
Brian D'Ostilio <briand787b@gmail.com>
2023-06-15 12:58:59 +0000

    removes all non-base deps and re-exports deps

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-import httpx
+# import httpx
 
 def main():
-    r = httpx.get('https://www.mallofgeorgiaford.com/used-inventory/index.htm?bodyStyle=Truck&normalDriveLine=4WD&internetPrice=10000-40000')
-    print(f'{r.text}')
+#    r = httpx.get('https://www.mallofgeorgiaford.com/used-inventory/index.htm?bodyStyle=Truck&normalDriveLine=4WD&internetPrice=10000-40000')
+#    print(f'{r.text}')
+
+    print('hello world, I have no dependencies')
 
 if __name__ == '__main__':
     main()
